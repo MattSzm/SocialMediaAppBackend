85448ccab35b4d7d8aac025c2568b96d0227c6f2
MattSzm <mateuszszmal@gmail.com>
2020-08-22 08:54:08 +0200

    created share/comment connectors

--- tweet/models.py
+++ tweet/models.py
@@ -8,13 +8,23 @@ class Tweet(models.Model):
     uuid = models.UUIDField(default=uuid.uuid4, editable=False,
                             unique=True, db_index=True)
     created = models.DateTimeField(auto_now_add=True)
+
     likes = models.ManyToManyField(get_user_model(),
                                    through='LikeConnector',
                                    related_name='liked_tweets')
-
+    shares = models.ManyToManyField(get_user_model(),
+                                    through='ShareConnector',
+                                    related_name='shared_tweets')
+    comments = models.ManyToManyField(get_user_model(),
+                                      through='CommentConnector',
+                                      related_name='commented_tweets')
 
     def __str__(self):
         return str(self.uuid)
+
+    @property
+    def number_of_comments(self):
+        return len(self.comments.all())
 
 
 
@@ -25,3 +35,23 @@ class LikeConnector(models.Model):
     tweet = models.ForeignKey(Tweet,
                               related_name='like_connector_tweet',
                               on_delete=models.CASCADE)
+
+class ShareConnector(models.Model):
+    account = models.ForeignKey(get_user_model(),
+                                related_name='share_connector_account',
+                                on_delete=models.CASCADE)
+    tweet = models.ForeignKey(Tweet,
+                              related_name='share_connector_tweet',
+                              on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_now_add=True)
+
+class CommentConnector(models.Model):
+    account = models.ForeignKey(get_user_model(),
+                                related_name='comment_connector_account',
+                                on_delete=models.CASCADE)
+    tweet = models.ForeignKey(Tweet,
+                              related_name='comment_connector_tweet',
+                              on_delete=models.CASCADE)
+    comment_content = models.CharField(max_length=280, blank=False)
+    created = models.DateTimeField(auto_now_add=True)
+
