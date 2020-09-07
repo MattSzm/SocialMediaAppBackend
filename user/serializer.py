90c94990e7edeb8e0eb86a63e0d917f8752228ea
MattSzm <mateuszszmal@gmail.com>
2020-09-08 01:22:33 +0200

    UserEditSerializer got more readonly fields, may be useful

--- user/serializer.py
+++ user/serializer.py
@@ -14,9 +14,12 @@ class UserSerializer(serializers.ModelSerializer):
 
 class UserEditSerializer(serializers.ModelSerializer):
     username_readonly = serializers.ReadOnlyField(source='username')
+    number_following = serializers.ReadOnlyField(source='num_of_following')
+    number_followers = serializers.ReadOnlyField(source='num_of_followers')
 
     class Meta:
         model = User
         fields = ('id', 'uuid', 'username_readonly',
-                  'username_displayed', 'photo')
+                  'username_displayed', 'photo',
+                  'number_following', 'number_followers')
 
