76ead70da99c5b06974b3fa2eefafb6f4c35dd1e
MattSzm <mateuszszmal@gmail.com>
2020-09-16 02:04:22 +0200

    cors applied

--- twitterclonebackend/settings.py
+++ twitterclonebackend/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'knox',
     'drf_yasg',
+    'corsheaders',
 
     'user.apps.UserConfig',
     'tweet.apps.TweetConfig'
@@ -45,6 +46,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -145,3 +147,7 @@ REST_FRAMEWORK = {
 
 NEWSFEED_SIZE = 10
 
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+    "http://localhost:8000",
+]
